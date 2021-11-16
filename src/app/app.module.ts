f406cc5c1802fe164a6eda0a08782cbaa592fd7d
GetSetAbhi <araina97@gmail.com>
2021-11-16 15:26:37 +0530

    3 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,19 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { BarComponent } from './bar/bar.component';
+import { PieComponent } from './pie/pie.component';
+import { ScatterComponent } from './scatter/scatter.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    BarComponent,
+    PieComponent,
+    ScatterComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
